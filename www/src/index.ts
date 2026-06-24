85745a82fae28ec0ef1c1bb60acbc818099eadc5
Jay Li <jn.sapiens@gmail.com>
2026-06-25 00:48:17 +0900

    feat: used shared css

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,4 +1,8 @@
 import { initSimulator } from "./sim";
 import "./serial_monitor";
 
+// Import shared CSS
+import "chimpanzee-ui/overlay.css";
+import "chimpanzee-ui/serial-monitor.css";
+
 initSimulator();
