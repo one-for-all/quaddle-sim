957937ad42285b9d0b3e673f900bf7b0ac83decf
Jay Li <jn.sapiens@gmail.com>
2026-06-25 20:33:45 +0900

    refactor: use serial monitor from lib

--- www/src/index.ts
+++ www/src/index.ts
@@ -1,7 +1,10 @@
 import { initSimulator } from "./sim";
-import "./serial_monitor";
+import { getSimulator } from "./sim";
+import { createSerialMonitorPanel } from "chimpanzee-ui";
 
 // Import shared CSS
 import "chimpanzee-ui/index.css";
+
+const panel = createSerialMonitorPanel({ getSimulator });
 
 initSimulator();
