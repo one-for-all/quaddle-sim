bb733e0f7c222e0ce33345d5bbbeb7aee171a311
Jay Li <jn.sapiens@gmail.com>
2026-07-15 00:20:29 +0900

    feat: add motion buttons

--- www/src/index.ts
+++ www/src/index.ts
@@ -7,9 +7,20 @@ import {
   createSerialMonitorPanel,
   setupResize,
   updateUIforMode,
+  createMotionButton,
 } from "chimpanzee-ui";
 
 initSimulator();
 const panel = createSerialMonitorPanel({ getSimulator });
 updateUIforMode();
 setupResize();
+
+const motions = [
+  { payload: "kbbbk", label: "Back Walk" },
+  { payload: "kbiped", label: "Biped Walk" },
+  { payload: "kbkL", label: "Backward Left Turn" },
+  { payload: "kboat", label: "Boat" },
+];
+for (const { payload, label } of motions) {
+  createMotionButton({ getSimulator }, payload, label);
+}
